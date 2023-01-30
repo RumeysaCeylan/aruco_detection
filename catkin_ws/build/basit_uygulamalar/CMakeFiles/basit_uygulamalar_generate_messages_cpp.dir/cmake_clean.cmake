3fd27c7c0c0b419fda1b1bb60c1ec1180b284c5d
Rümeysa Ceylan <59581456+RumeysaCeylan@users.noreply.github.com>
2023-01-30 11:28:21 +0300

    Add files via upload

---
+++ catkin_ws/build/basit_uygulamalar/CMakeFiles/basit_uygulamalar_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basit_uygulamalar_generate_messages_cpp"
+  "/home/rumeysa/catkin_ws/devel/include/basit_uygulamalar/Mesafe.h"
+  "/home/rumeysa/catkin_ws/devel/include/basit_uygulamalar/CemberHareket.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/basit_uygulamalar_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/basit_uygulamalar/CMakeFiles/basit_uygulamalar_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basit_uygulamalar_generate_messages_eus"
+  "/home/rumeysa/catkin_ws/devel/share/roseus/ros/basit_uygulamalar/msg/Mesafe.l"
+  "/home/rumeysa/catkin_ws/devel/share/roseus/ros/basit_uygulamalar/srv/CemberHareket.l"
+  "/home/rumeysa/catkin_ws/devel/share/roseus/ros/basit_uygulamalar/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/basit_uygulamalar_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
