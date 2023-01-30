666cc88f5de9fca68bb84f2af4b1ab5ff30b4c34
Rümeysa Ceylan <59581456+RumeysaCeylan@users.noreply.github.com>
2023-01-30 11:33:33 +0300

    Add files via upload

---
+++ catkin_ws/build/opencv_uygulamalar/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rumeysa/catkin_ws/src/opencv_uygulamalar
+# Build directory: /home/rumeysa/catkin_ws/build/opencv_uygulamalar
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
